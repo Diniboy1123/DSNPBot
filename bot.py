7283fe0ea2133ff3697921c21aa9303f476f0c52
Diniboy1123 <Diniboy1123@users.noreply.github.com>
2022-10-17 09:18:14 +0200

    bot: add regions command

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
+from disney import DisneyPlus
 import config
 
 API_TOKEN = config.token
@@ -24,6 +25,16 @@ async def send_welcome(message: types.Message):
         "Hi!\nI'm a Disney+ information grabber bot! If you want to know which series are available at a specific region, this bot is for you!"
     )
 
+@dp.message_handler(commands=["regions"])
+async def send_regions(message: types.Message):
+    """
+    This handler will be called when user sends `/regions` command
+    """
+    await message.reply(
+        f"Here are the regions available ({len(bot.disney.regions)}): " + ", ".join(bot.disney.regions)
+    )
+
 if __name__ == '__main__':
     bot.logging = logging.getLogger()
-    executor.start_polling(dp, skip_updates=True)
+    bot.disney = DisneyPlus(bot)
+    executor.start_polling(dp, skip_updates=True, on_startup=bot.disney.init_session)
